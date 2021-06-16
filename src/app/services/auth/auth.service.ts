ca51f6668e13a92ef548048bf3adb36720964bd2
Alex Reinoso <ralexjessiel@gmail.com>
2021-06-16 17:10:57 -0500

    verificar si hay un usuario ya loggeado con el servicio

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,27 @@
+/* eslint-disable no-trailing-spaces */
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, of } from 'rxjs';
+import { first, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  public user: Observable<any>;
+
+  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) {
+    this.user = this.afAuth.authState.pipe(
+      switchMap(user=>{
+        if(user){
+          return this.afs.doc<any>(`users/${user.uid}`).valueChanges();
+        }else {
+          return of(null);
+        }
+      })
+    );
+  }
+
 }
