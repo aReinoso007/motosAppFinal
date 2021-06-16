c48e9886127109af31cc1c9f7cff264d8d68fb43
Alex Reinoso <ralexjessiel@gmail.com>
2021-06-16 18:24:55 -0500

    servicio y formulario de registro

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,8 @@
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserOptions } from 'src/app/interfaces/user-options';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  login: UserOptions = { name:'', email:'', password:''};
+  submitted = false;
+  constructor(public authService: AuthService, public router: Router) { }
 
   ngOnInit() {
+  }
+
+  async onLogin(form: NgForm){
+    this.submitted = true;
+    if(form.valid){
+      const error = await this.authService.emailPasswordLogin(this.login.email, this.login.password);
+      if(error === undefined ){
+        console.log('login exitoso, redirigiendo a dashboard de usuario');
+        this.router.navigateByUrl('app/tabs');
+      }else{
+        alert(JSON.stringify(error));
+      }
+    }
+  }
+
+  onSignup() {
+    this.router.navigateByUrl('/signup');
   }
 
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,13 @@
+/* eslint-disable no-throw-literal */
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/quotes */
 /* eslint-disable no-trailing-spaces */
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable, of } from 'rxjs';
 import { first, switchMap } from 'rxjs/operators';
+import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -42,6 +46,49 @@ export class AuthService {
     return this.afs
           .collection('users', ref=> ref.where('email', '==', email))
           .valueChanges().pipe(first()).toPromise();
+  }
+
+  async emailPasswordLogin(email: string, password: string){
+    try{
+      const credentials = firebase.default.auth.EmailAuthProvider.credential(email, password);
+      const firebaseUser = await firebase.default.auth().signInWithCredential(credentials);
+      return await this.updateUserData(firebaseUser.user, 'email');
+    }catch(err){
+      return err;
+    }
+  }
+
+  async updateUserData(userTemp: any, provider: any){
+    const doc: any = await this.userExists(userTemp.email);
+    let data: any;
+    const user: any = JSON.parse(JSON.stringify(userTemp));
+
+    if(doc == null || doc ===""){
+      //creamos la cuenta del usuario
+      data ={
+        uid: user.uid,
+        rol: 'user',
+        email: user.email || null,
+        displayName: user.displayName || '',
+        photoURL: user.photoURL,
+        provider,
+        lastLogin: new Date(Number(user.lastLoginAt)) || new Date(),
+        createdAt: new Date(Number(user.createdAt)) || new Date()
+      };
+    }else if( doc.active === false ){
+     throw { error_code: 999, error_message: 'Acceso denegado' };
+    }else{
+      //se actualiza la cuenta
+      data ={
+        uid: user.uid,
+        email: user.email || null,
+        provider,
+        lastLogin: new Date(Number(user.lastLoginAt)) || new Date()
+      };
+    }
+    console.log("data: ", JSON.stringify(data));
+    const userRef = this.afs.collection<any>('users');
+    return userRef.doc(`${user.uid}`).set(data, {merge: true});
   }
 
 }
