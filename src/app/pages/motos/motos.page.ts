c416c0d5b22e6fb867c7f689636cd92d8617b9fd
Alex Reinoso <ralexjessiel@gmail.com>
2021-06-23 15:32:44 -0500

    validando accesos a informacion

--- src/app/pages/motos/motos.page.ts
+++ src/app/pages/motos/motos.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MotosService } from 'src/app/services/motos/motos.service';
 
 @Component({
   selector: 'app-motos',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./motos.page.scss'],
 })
 export class MotosPage implements OnInit {
-
-  constructor() { }
-
+  motosList: any[] = [];
+  userData = JSON.parse(JSON.stringify(localStorage.getItem('user')));
+  constructor(private motosService: MotosService, private router: Router) { }
   ngOnInit() {
+    console.log('uid usuario loggeado: ', this.userData);
+    this.checkUser();
+    this.getMotosList();
+    console.log('motos recuperadas: ', this.motosList);
+  }
+
+  getMotosList(){
+    try{
+      this.motosService.getUserMotos(this.userData.uid)
+      .subscribe(data=>{
+        this.motosList = JSON.parse(JSON.stringify(data));
+      });
+    }catch(err){
+      throw console.error('error retrieving user motoslist: ', err.message);
+    }
+  }
+
+  checkUser(){
+    console.log('verificando...');
+    if(this.userData === undefined){
+      alert('Login to access dashboard');
+      this.router.navigateByUrl('/login');
+      return false;
+    }else{
+      return true;
+    }
   }
 
 }
