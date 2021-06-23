6a9083d1eb04bd09d226973c3e8363622263c50c
Alex Reinoso <ralexjessiel@gmail.com>
2021-06-23 14:24:44 -0500

    servicio para cargar imagen de moto

--- src/app/services/motos/motos.service.ts
+++ src/app/services/motos/motos.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MotosService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore, private storage: AngularFireStorage) { }
+
+  async uploadPhoto(id: string, file: any): Promise<any>{
+    if(file && file.length){
+      try{
+        const task = await this.storage.ref('motos').child(id).put(file[0]);
+        return this.storage.ref(`motos/${id}`).getDownloadURL().toPromise();
+      }catch(err){
+        console.error('Error uploading file: ', err.message);
+      }
+    }
+  }
 }
