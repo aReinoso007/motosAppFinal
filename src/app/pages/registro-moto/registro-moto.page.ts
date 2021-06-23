b2aecd7a4b408bf1d6e72ecc895026d742582dc6
Alex Reinoso <ralexjessiel@gmail.com>
2021-06-23 15:11:28 -0500

    servico para obtener motos del usuario

--- src/app/pages/registro-moto/registro-moto.page.ts
+++ src/app/pages/registro-moto/registro-moto.page.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-trailing-spaces */
 import { Component, OnInit } from '@angular/core';
 import { Moto } from 'src/app/model/moto.model';
+import { MotosService } from 'src/app/services/motos/motos.service';
 
 @Component({
   selector: 'app-registro-moto',
@@ -8,14 +9,33 @@ import { Moto } from 'src/app/model/moto.model';
   styleUrls: ['./registro-moto.page.scss'],
 })
 export class RegistroMotoPage implements OnInit {
+  
   moto: Moto = new Moto();
-  constructor() { }
+  selectedFile: any;
+  usuario = JSON.parse(JSON.stringify(localStorage.getItem('user')));
+  constructor(private motosService: MotosService) { }
 
   ngOnInit() {
+    console.log('nombre: ', this.usuario.name, ' uid: ', this.usuario.uid);
   }
 
-  addMoto(moto: Moto) {
-    
+  async addMoto() {
+    /*Obtener uid del usuario */
+    const user = JSON.parse(localStorage.getItem('user'));
+    if(user){
+      const uidUser = user.uid;
+      const imageURL = await this.motosService.uploadPhoto(uidUser, this.selectedFile);
+      this.motosService.addUserMoto(this.moto, uidUser);
+      alert('Moto ingresada exitosamente');
+      this.moto = new Moto();
+      /*Redirigir a la lista de motos y mandar a refrescar la lista de las motos */
+    }else{
+      throw console.error('Usuario no loggeado');
+    }
+  }
+
+  chooseFile(event){
+    this.selectedFile = event.target.files;
   }
 
 }
--- src/app/services/motos/motos.service.ts
+++ src/app/services/motos/motos.service.ts
@@ -1,6 +1,8 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireStorage } from '@angular/fire/storage';
+import { Moto } from 'src/app/model/moto.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,7 @@ export class MotosService {
 
   constructor(private afs: AngularFirestore, private storage: AngularFireStorage) { }
 
-  async uploadPhoto(id: string, file: any): Promise<any>{
+  async uploadPhoto(id, file: any): Promise<any>{
     if(file && file.length){
       try{
         const task = await this.storage.ref('motos').child(id).put(file[0]);
@@ -18,5 +20,19 @@ export class MotosService {
         console.error('Error uploading file: ', err.message);
       }
     }
+  }
+  /*Refactor to get user uid from moto */
+  addUserMoto(moto: Moto, uidUser: string){
+    try{
+      const refUser = this.afs.collection('users');
+      const param = JSON.parse(JSON.stringify(moto));
+      refUser.doc(uidUser).collection<any>('motos').doc(moto.uid).set(param, {merge: true});
+    }catch(err){
+      throw console.error('Error insertando moto de usuario: ', err.message);
+    }
+  }
+
+  getUserMotos(uid: string): Observable<any[]>{
+    return this.afs.collection('users').doc(uid).collection('motos').valueChanges();
   }
 }
